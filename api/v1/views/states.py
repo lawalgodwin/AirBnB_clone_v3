b21e50b3f76686c9c2cfd245ea5d028b65ef6107
lawalgodwin <godwinl200@gmail.com>
2023-07-19 19:24:19 +0100

    Implement CRUD on State Resource

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -2,23 +2,58 @@
 """This module contains the view functions that handle all request
    on State resource
 """
-from flask import jsonify
+from flask import jsonify, request, abort
 from models import storage
 from models.state import State
 from api.v1.views import app_views
 
 
-@app_views.route('/states', strict_slashes=False)
-def getAllStates():
+@app_views.route('/states', strict_slashes=False, methods=["GET", "POST"])
+def States():
     """retrieve all states"""
-    allStates = [state.to_dict() for state in storage.all("State").values()]
-    print(allStates)
-    return jsonify(allStates)
+    if request.method == 'GET':
+        allStates = [s.to_dict() for s in storage.all("State").values()]
+        return jsonify(allStates)
 
-@app_views.route('/states/<uuid:state_id>', strict_slashes=False)
-def getState(state_id):
+    if request.method == 'POST':
+        if not request.is_json:
+            abort(400, description="Not a JSON")
+        data = request.get_json()
+        if "name" not in data:
+            abort(400, description="missing name")
+        else:
+            new_state = State(**data)
+            storage.new(new_state)
+            storage.save()
+            return jsonify(new_state.to_dict()), 201
+
+
+@app_views.route('/states/<uuid:state_id>',
+                 strict_slashes=False,
+                 methods=["GET", "DELETE", "PUT"])
+def UniqueState(state_id):
     """Get the state with the specified id"""
-    state = storage.get(State, state_id)
-    if state:
-        return jsonify(state.to_dict()), 200
-    return jsonify({}), 404
+    target_state = storage.get(State, state_id)
+    if request.method == "GET":
+        if target_state:
+            return jsonify(target_state.to_dict()), 200
+        abort(404)
+
+    if request.method == "DELETE":
+        if target_state is None:
+            abort(404)
+        target_state.delete()
+        storage.save()
+        return jsonify({}), 200
+
+    if request.method == "PUT":
+        if target_state is None:
+            abort(404)
+        if not request.is_json:
+            abort(400, description="Not a JSON")
+        data = request.get_json()
+        for k, v in data.items():
+            if k not in ["id", "created_at", "updated_at"]:
+                setattr(target_state, k, v)
+        target_state.save()
+        return jsonify(target_state.to_dict()), 200
